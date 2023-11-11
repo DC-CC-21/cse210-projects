0a4c69660890ce402cbc13a1161dbc52741f3d7b
DC-CC-21 <handstands1000@gmail.com>
2023-11-11 13:09:35 -0500

    updated code

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -8,8 +8,11 @@ class Program
         /*
             Exceeds Expectations:
                 Wrote a class for handling multiple choice questions in the terminal with arrow keys
-                Wrote a function for saving entries so that they can be opened in Excel
+                Wrote a function for saving entries as a csv that can be opened in Excel
                 Wrote a function inside of my ConsoleOptions class for handling escapes during keyboard input
+                Added functionality for the user to choose if the want to see the prompt
+                Added functionality for the user to delete an entry
+                
         */
 
         Journal journal = new();
