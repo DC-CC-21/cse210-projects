d6a1003c945b8368167c9648ba40ba75ffc7120c
DC-CC-21 <handstands1000@gmail.com>
2023-11-14 14:15:56 -0500

    finished learning activity 3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction a = new();
+        Console.WriteLine(a.GetFractionString());
+        Console.WriteLine(a.GetDecimalValue());
+
+        Fraction b = new(5);
+        Console.WriteLine(b.GetFractionString());
+        Console.WriteLine(b.GetDecimalValue());
+
+        Fraction c = new(3, 4);
+        Console.WriteLine(c.GetFractionString());
+        Console.WriteLine(c.GetDecimalValue());
+
+        c.SetNumerator(1);
+        c.SetDenominator(3);
+        Console.WriteLine(c.GetFractionString());
+        Console.WriteLine(c.GetDecimalValue());
+
     }
 }
