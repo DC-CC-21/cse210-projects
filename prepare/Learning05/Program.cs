fd0dadc4ea11e2a100320a9261da89ddd87fa660
DC-CC-21 <handstands1000@gmail.com>
2023-11-28 15:21:39 -0500

    completed learning activities

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new(10, "blue");
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
+
+        Rectangle rectangle = new(10, 5, "red");
+        Console.WriteLine(rectangle.GetColor());
+        Console.WriteLine(rectangle.GetArea());
+
+        Circle circle = new(3, "pink");
+        Console.WriteLine(circle.GetColor());
+        Console.WriteLine(circle.GetArea());
+
+        List<Shape> shapes = new(){
+            new Square(15, "orange"),
+            new Rectangle(2, 3, "black"),
+            new Circle(8, "yellow"),
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
