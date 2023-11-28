26b5f4e499fac648b6ecca82484157ae1da9668d
DC-CC-21 <handstands1000@gmail.com>
2023-11-28 15:53:19 -0500

    added comments

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,28 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
+        // test square
         Square square = new(10, "blue");
         Console.WriteLine(square.GetColor());
         Console.WriteLine(square.GetArea());
 
+        // test rectangle
         Rectangle rectangle = new(10, 5, "red");
         Console.WriteLine(rectangle.GetColor());
         Console.WriteLine(rectangle.GetArea());
 
+        // tesh circle
         Circle circle = new(3, "pink");
         Console.WriteLine(circle.GetColor());
         Console.WriteLine(circle.GetArea());
 
+        // add shapes to list
         List<Shape> shapes = new(){
             new Square(15, "orange"),
             new Rectangle(2, 3, "black"),
             new Circle(8, "yellow"),
         };
 
+        // run GetColor and GetArea on shapes in the list
         foreach (Shape shape in shapes)
         {
-            Console.WriteLine(shape.GetColor());
-            Console.WriteLine(shape.GetArea());
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {shape.GetArea()}");
         }
     }
 }
